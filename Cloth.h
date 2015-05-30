f31596c3b04ae1e86cec79b00903e8ae5be10722
Kyle Piddington <kpiddy@gmail.com>
2015-05-30 14:42:08 -0700

    Added cloth h file, beginning work on cloth class

---
+++ Cloth.h
@@ -0,0 +1,53 @@
+#ifndef __CLOTH_H__
+#define __CLOTH_H__
+#include "glm/glm.hpp"
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#endif
+#ifdef __unix__
+#include <GL/glut.h>
+#endif
+#include <iostream>
+#include "GLSL.h"
+#include "glm/glm.hpp"
+#include <vector>
+
+typedef struct Vertex
+{
+   glm::vec3 position;
+   glm::vec3 velocity;
+}Vertex;
+
+
+typedef struct Triangle
+{
+   Vertex a;
+   Vertex b;
+   Vertex c;
+}Triangle;
+
+
+class Cloth
+{
+public:
+   Cloth(glm::vec2 size, glm::vec2 resolution);
+   ~Cloth();
+   void step();
+   void bind();
+   void draw(h_pos, h_nor);
+   Triangle getTriangle(int triangle);
+
+private:
+   GLuint posBufID;
+   GLuint norBufID;
+
+   int numTriangles;
+   std::vector<glm::vec3> vertex_position;
+   std::vector<glm::vec2> vertex_uv;
+   std::vector<glm::vec3> vertex_velocity;
+   std::vector<int> indices;
+
+
+
+};
+#endif
