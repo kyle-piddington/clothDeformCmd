95f36f67c79fd1c4af84676aedc456f428a94e93
Kyle Piddington <kpiddy@gmail.com>
2015-05-30 14:47:30 -0700

    Forgot words in cloth.cpp

---
+++ Cloth.cpp
@@ -0,0 +1,22 @@
+#import "Cloth.h"
+
+
+Cloth::Cloth(glm::vec2 size, glm::vec2 resolution)
+{
+   /*Cloth Generation*/
+}
+
+Cloth::init()
+{
+   /*Buffer generation*/
+}
+
+Cloth::step(float dt)
+{
+   /*Cloth simulation*/
+}
+
+Cloth::bind()
+{
+   /*update vertices on GPU*/
+}
